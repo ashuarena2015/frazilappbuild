2b300d5e98a95f8744c3400ee0f2ef2a8bcd1f6b
ashuarena <ashutosh.kumar@sendinblue.com>
2019-09-23 09:06:17 +0530

    frazilapp build files

---
+++ precache-manifest.ec611d865dcff412548b33f12f1488f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6286adb05eca610d36496c41be467acd",
+    "url": "/frazilapp/index.html"
+  },
+  {
+    "revision": "214eba46869637211147",
+    "url": "/frazilapp/static/css/main.a0abb6f0.chunk.css"
+  },
+  {
+    "revision": "773426a942ad526e9bc4",
+    "url": "/frazilapp/static/js/2.80b114f2.chunk.js"
+  },
+  {
+    "revision": "214eba46869637211147",
+    "url": "/frazilapp/static/js/main.792f3eda.chunk.js"
+  },
+  {
+    "revision": "7f30d97bc4dacb911771",
+    "url": "/frazilapp/static/js/runtime~main.176bb93e.js"
+  }
+]);
