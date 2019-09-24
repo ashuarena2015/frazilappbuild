3be89d72c62db6daeae74cb67449f9eb488b7e93
ashuarena <ashutosh.kumar@sendinblue.com>
2019-09-24 22:31:14 +0530

    remove www

---
+++ precache-manifest.005911ca79ba75c8de3c2aed71a369f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6122a74677461aff03cadc21193f9289",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46cd53c6a5713099d693",
+    "url": "/static/css/main.a0abb6f0.chunk.css"
+  },
+  {
+    "revision": "f0d350eb47b4ae48da97",
+    "url": "/static/js/2.8c4a1bbe.chunk.js"
+  },
+  {
+    "revision": "46cd53c6a5713099d693",
+    "url": "/static/js/main.ca7e36a6.chunk.js"
+  },
+  {
+    "revision": "b5be6e481a6fe173d752",
+    "url": "/static/js/runtime~main.32f72a5e.js"
+  }
+]);
